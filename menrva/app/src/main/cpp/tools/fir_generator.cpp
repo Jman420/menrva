dfe566ee51b3b8364e0c13a1da1bff9063779216
Justin Giannone <jgiannone@mdsol.com>
2018-12-06 15:44:00 -0500

    Adding FIR Generator

--- menrva/app/src/main/cpp/tools/fir_generator.cpp
+++ menrva/app/src/main/cpp/tools/fir_generator.cpp
@@ -1,6 +1,42 @@
-//
-// Created by jgiannone on 12/5/2018.
-//
+// Author : Jman420
 
+#include <cstdlib>
 #include "fir_generator.h"
 
+double* FIR_Generator::Create(int interpolationSize, double* frequencySamples, double* amplitudeSamples, int sampleSize) {
+    // Validate Frequency Samples
+    if (frequencySamples[0] != 0 || frequencySamples[sampleSize - 1] != 1) {
+        // TODO : Add Error Log Line re: Invalid Frequency Samples
+        return 0;
+    }
+    for (int freqCounter = 0; freqCounter < sampleSize - 1; freqCounter++) {
+        if (frequencySamples[freqCounter] >= frequencySamples[freqCounter + 1]) {
+            // TODO : Add Error Log Line re: Invalid Frequency Samples
+            return 0;
+        }
+    }
+
+    // Interpolate Amplitudes
+    double amplitudeIncrement;
+    int beginSegmentIndex, endSegmentIndex = 0;
+
+    double* interpolatedAmplitudes = (double*)malloc(sizeof(double)*interpolationSize);
+    interpolatedAmplitudes[0] = amplitudeSamples[0];
+
+    for (int amplitudeCounter = 0; amplitudeCounter < sampleSize - 1; amplitudeCounter++) {
+        endSegmentIndex = (int)(frequencySamples[amplitudeCounter + 1] * interpolationSize) - 1;
+        if (beginSegmentIndex < 0 || endSegmentIndex > interpolationSize) {
+            // TODO : Add Error Log Line re: Amplitude change out of bounds
+            return 0;
+        }
+
+        for (int elementCounter = beginSegmentIndex; elementCounter < endSegmentIndex; elementCounter++) {
+            amplitudeIncrement = (double)(elementCounter - beginSegmentIndex) / (double)(endSegmentIndex - beginSegmentIndex);
+            interpolatedAmplitudes[elementCounter] = amplitudeIncrement * amplitudeSamples[amplitudeCounter + 1] + (1.0 - amplitudeIncrement) * amplitudeSamples[amplitudeCounter];
+        }
+
+        beginSegmentIndex = endSegmentIndex + 1;
+    }
+
+
+}
--- menrva/app/src/main/cpp/tools/fir_generator.h
+++ menrva/app/src/main/cpp/tools/fir_generator.h
@@ -1,6 +1,4 @@
-//
-// Created by jgiannone on 12/5/2018.
-//
+// Author : Jman420
 
 #ifndef MENRVA_FIR_GENERATOR_H
 #define MENRVA_FIR_GENERATOR_H
@@ -8,10 +6,15 @@
 
 class FIR_Generator {
 public:
+    /*
+     * Create() - Returns a Finite Impulse Response of size 'interpolationSize' based on the provided
+     * Frequency Samples & Amplitudes.
+     * interpolationSize - Size of resulting FIR Filter (number of elements in the filter; 2048 or 4096)
+     * frequencySamples - Array of Frequency values to interpolate
+     * amplitudeSamples - Array of Amplitude values to interpolate
+     * sampleSize - Length of Frequency & Amplitude Arrays
+     */
     double* Create(int interpolationSize, double* frequencySamples, double* amplitudeSamples, int sampleSize);
-
-private:
-
 };
 
 
