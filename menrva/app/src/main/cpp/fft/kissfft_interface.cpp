3385049f187c7bed6cba01792d6b1be6e60cbd5d
Justin Giannone <jgiannone@mdsol.com>
2019-01-24 09:23:55 -0500

    Fix KissFFT Initialization Bug

--- menrva/app/src/main/cpp/fft/kissfft_interface.cpp
+++ menrva/app/src/main/cpp/fft/kissfft_interface.cpp
@@ -39,15 +39,15 @@ size_t KissFftInterface::Initialize(size_t signalSize, size_t componentSize) {
     if (cachedPlansIterator != _PlansCache->end()) {
         _Logger->WriteLog("Successfully found Cached FFT Plans for Initialization!", LOG_SENDER, __func__);
         _Plans = cachedPlansIterator->second;
-        return componentSize;
     }
-
-    _Logger->WriteLog("Calculating and Caching FFT Plans for Cache Key (%s)...", LOG_SENDER, __func__, plansKeyC);
-    _Plans.RealToComplexPlan = KissFftCreatePlan(signalSize, 0, 0, 0);
-    _Plans.ComplexToRealPlan = KissFftCreatePlan(signalSize, 1, 0, 0);
-    _PlansCache->insert( { plansKey, _Plans } );
-    _Logger->WriteLog("Successfully Calculated and Cached FFT Plans for Cache Key (%s).", LOG_SENDER, __func__, plansKeyC);
-
+    else {
+        _Logger->WriteLog("Calculating and Caching FFT Plans for Cache Key (%s)...", LOG_SENDER, __func__, plansKeyC);
+        _Plans.RealToComplexPlan = KissFftCreatePlan(signalSize, 0, 0, 0);
+        _Plans.ComplexToRealPlan = KissFftCreatePlan(signalSize, 1, 0, 0);
+        _PlansCache->insert({plansKey, _Plans});
+        _Logger->WriteLog("Successfully Calculated and Cached FFT Plans for Cache Key (%s).", LOG_SENDER, __func__, plansKeyC);
+    }
+    
     _Logger->WriteLog("Allocating KissFFT Components Buffer...", LOG_SENDER, __func__);
     _ComponentsBuffer = (kiss_fft_cpx*)malloc(sizeof(kiss_fft_cpx) * componentSize);
 
