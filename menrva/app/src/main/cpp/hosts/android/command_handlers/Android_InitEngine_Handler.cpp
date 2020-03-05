53b4f707d6cc9f5db626cd6aa865ed493ae57f94
Justin Giannone <jgiannone@mdsol.com>
2020-03-05 11:17:22 -0500

    Add remaining Android Host Specific Command Handlers

---
+++ menrva/app/src/main/cpp/hosts/android/command_handlers/Android_InitEngine_Handler.cpp
@@ -0,0 +1,44 @@
+/* Menrva - Over-Engineered Tunable Android Audio Effects
+ * Copyright (C) 2020 Justin Giannone (aka Jman420)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "Android_InitEngine_Handler.h"
+#include "../commands/AndroidHost_Generic_Command.h"
+
+Android_InitEngine_Handler::Android_InitEngine_Handler(LoggerBase* logger)
+        : CommandHandlerBase(new AndroidHost_Generic_Command(), logger, __PRETTY_FUNCTION__) {}
+
+void Android_InitEngine_Handler::Execute(MenrvaModuleContext& context) {
+    _Logger->WriteLog("Initializing Module Context...", LOG_SENDER, __func__);
+
+    if (context.ModuleStatus > MenrvaModuleStatus::INITIALIZING) {
+        _Logger->WriteLog("Module Context already Initialized!", LOG_SENDER, __func__);
+        return;
+    }
+
+    context.ModuleStatus = MenrvaModuleStatus::INITIALIZING;
+    // TODO : Configure any necessary default parameters
+    //_Logger->WriteLog("Setting up Menrva Effects Engine Parameters...", logPrefix);
+
+    context.ModuleStatus = MenrvaModuleStatus::READY;
+    _Logger->WriteLog("Successfully Initialized Menrva Context!", LOG_SENDER, __func__);
+}
+
+uint32_t Android_InitEngine_Handler::SerializeResponse(void* responseBuffer) {
+    *(int*)responseBuffer = 0;
+    return sizeof(int);
+}
