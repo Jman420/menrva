8bf000f1b5300805d52cd530aac5667cc12c04a4
Justin Giannone <jgiannone@mdsol.com>
2020-03-24 11:42:32 -0400

    Add TestHarness CMake Project

---
+++ hosts/testHarness/src/MenrvaTestHarness.cpp
@@ -0,0 +1,12 @@
+﻿// MenrvaTestHarness.cpp : Defines the entry point for the application.
+//
+
+#include "MenrvaTestHarness.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
